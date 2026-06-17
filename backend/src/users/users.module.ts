a00b2dd2b583a0c55cc2d651acc1170c1df9e8de
aquammarine <mrguysy@gmail.com>
2026-06-17 20:29:06 +0300

    style: typo in name of repository

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersRepository } from './user.repository';
+import { UsersRepository } from './users.repository';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UsersRepository } from './user.repository';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
