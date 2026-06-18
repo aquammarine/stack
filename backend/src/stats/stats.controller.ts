e5b212b83905aff0cc01425f678c1f5aa3df835e
aquammarine <mrguysy@gmail.com>
2026-06-18 23:19:04 +0300

    feat: add endpoint and implement service for stats

--- backend/src/stats/stats.controller.ts
+++ backend/src/stats/stats.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { CurrentUser } from '../common/decorators/curent-user.decorator';
+import type { JwtPayload } from '../common/types/user.types';
+import { StatsService } from './stats.service';
 
 @Controller('stats')
-export class StatsController {}
+@UseGuards(JwtAuthGuard)
+export class StatsController {
+  constructor(private readonly statsService: StatsService) {}
+
+  @Get()
+  getGlobalStats(@CurrentUser() user: JwtPayload) {
+    return this.statsService.getGlobalStats(user.id);
+  }
+}
--- backend/src/stats/stats.service.ts
+++ backend/src/stats/stats.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { StatsRepository } from './stats.repository';
 
 @Injectable()
-export class StatsService {}
+export class StatsService {
+  constructor(private readonly statsRepository: StatsRepository) {}
+
+  async getGlobalStats(userId: string) {
+    if (!userId) throw new BadRequestException('Invalid userId');
+
+    const [globalStats, streak] = await Promise.all([
+      this.statsRepository.getGlobalStats(userId),
+      this.calculateStreak(userId),
+    ]);
+
+    return { ...globalStats, streak };
+  }
+
+  private async calculateStreak(userId: string) {
+    if (!userId) throw new BadRequestException('Invalid userId');
+
+    const dates = await this.statsRepository.getReviewDates(userId);
+    const reviewDates = new Set(
+      dates.map((date) => date.reviewedAt.toISOString().split('T')[0]),
+    );
+
+    const day = new Date();
+
+    let streak = 0;
+    for (let i = 0; i < 365; i++) {
+      const dateSrt = day.toISOString().split('T')[0];
+      if (!reviewDates.has(dateSrt)) {
+        break;
+      }
+      streak++;
+      day.setDate(day.getDate() - 1);
+    }
+
+    return streak;
+  }
+}
