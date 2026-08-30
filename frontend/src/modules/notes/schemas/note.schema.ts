c2fa012df92811e5d396c328d1f52568a3189c20
aquammarine <mrguysy@gmail.com>
2026-08-30 14:20:57 +0300

    feat: add notes schema

---
+++ frontend/src/modules/notes/schemas/note.schema.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+import { NoteType } from "../types";
+
+const noteSchema = z.object({
+  title: z.string().min(1).max(200),
+  noteType: z.enum(NoteType).optional(),
+  content: z.string().min(1),
+  sourceUrl: z.string().url().optional(),
+});
+
+export const createNoteSchema = noteSchema;
+export const updateNoteSchema = noteSchema.partial();
+
+export type CreateNoteFormValues = z.infer<typeof createNoteSchema>;
+export type updateNoteFormValues = z.infer<typeof updateNoteSchema>;
