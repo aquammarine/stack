54d2780e41b73dcc7e56153849b82fd7cc088cd9
aquammarine <mrguysy@gmail.com>
2026-08-30 13:57:17 +0300

    feat: add response interceptor

--- frontend/src/shared/lib/axios.ts
+++ frontend/src/shared/lib/axios.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { type AxiosResponse } from "axios";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -7,3 +7,21 @@ export const api = axios.create({
   },
   withCredentials: true,
 });
+
+let refreshPromise: Promise<AxiosResponse> | null = null;
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status !== 401 || error.config._retry) {
+      return Promise.reject(error);
+    }
+    error.config._retry = true;
+
+    refreshPromise ??= api
+      .post("/auth/refresh")
+      .finally(() => (refreshPromise = null));
+    await refreshPromise;
+    return api(error.config);
+  },
+);
